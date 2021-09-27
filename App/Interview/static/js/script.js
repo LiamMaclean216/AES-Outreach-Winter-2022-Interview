67027c16c5500e169c9ed27cf618e89bd2de3d50
LiamMaclean216 <lmaclean234@gmaill.com>
2021-09-26 21:39:29 -0400

    error catching

--- App/Interview/static/js/script.js
+++ App/Interview/static/js/script.js
@@ -7,6 +7,7 @@ document.querySelector("#create").addEventListener("submit", function(e) {
     let data = JSON.stringify({"list_ids" : this.querySelector("#door_id_list").value.replace(" ", "").split(",")})
     let url = `/create/`
 
+    this.querySelector("#access_key").innerHTML = ``;
     document.querySelector("#b_sent").innerHTML = `Sending POST request to url ${url} with body ${data}`
 
     let self = this
@@ -18,10 +19,15 @@ document.querySelector("#create").addEventListener("submit", function(e) {
         },
         body: data
 
-    }).then((response) => response.json()
-    ) .then(function (response) {
-        self.querySelector("#access_key").innerHTML = `Access Token : ${response["access_token"]}`
-        document.querySelector("#b_received").innerHTML = `Received : ${JSON.stringify(response)}`
+    }).then((response) => {
+        document.querySelector("#b_received").innerHTML = `Recieved response status ${response.statusText}`
+        return response.json();
+    }) .then(function (response) {
+        self.querySelector("#access_key").innerHTML = `Access Token : ${response["access_token"]}`;
+        document.querySelector("#b_received").innerHTML = `Received : ${JSON.stringify(response)}`;
+    }).catch((error) => {
+        console.log(error)
+        self.querySelector("#access_key").innerHTML = `Invalid input`
     })
 
 });
@@ -38,11 +44,14 @@ document.querySelector("#validate").addEventListener("submit", function(e) {
     fetch(url, {
         method: 'GET',
 
-    }).then((response) => response.json()
-    ) .then(function (response) {
-        self.querySelector("#is_valid").innerHTML = response["is_valid"] ? "valid" : "invalid"
-
+    }).then((response) => {
+        document.querySelector("#b_received").innerHTML = `Recieved response status ${response.statusText}`
+        return response.json();
+    }) .then(function (response) {
+        self.querySelector("#is_valid").innerHTML = response["is_valid"] ? "Valid token" : "Invalid token"
         document.querySelector("#b_received").innerHTML = `Received : ${JSON.stringify(response)}`
+    }).catch((error) => {
+        self.querySelector("#is_valid").innerHTML = `Invalid input`
     })
 
 });
