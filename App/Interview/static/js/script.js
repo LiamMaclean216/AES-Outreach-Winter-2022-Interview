db608258f125c7b841ff1d267eba23e9675a4d88
LiamMaclean216 <lmaclean234@gmaill.com>
2021-09-26 21:04:46 -0400

    create

--- App/Interview/static/js/script.js
+++ App/Interview/static/js/script.js
@@ -1,1 +1,24 @@
-console.log("sd;kjgflksdjfh;k")
+
+const csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+
+
+document.querySelector("#create").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let data = {"list_ids" : this.querySelector("#door_id_list").value.replace(" ", "").split(",")}
+    
+    let self = this
+    fetch(`/create/`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify(data)
+
+    }).then((response) => response.json()
+    ) .then(function (response) {
+        self.querySelector("#access_key").innerHTML = `Access Token : ${response["access_token"]}`
+        
+    })
+
+});
--- App/Interview/views.py
+++ App/Interview/views.py
@@ -6,18 +6,23 @@ from .models import *
 
 from django.http import JsonResponse
 
+import json
 
 # Create your views here.
 
 def index(request):
     return render(request, 'Interview/index.html')
 
-def create(request, list_ids):
-    hex = token_hex(16)
-    for i in list_ids:
-        Door.objects.create(door_id = i, token_hex = hex)
+def create(request):
+    print(request)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+        hex = token_hex(16)
+        for i in data["list_ids"]:
+            Door.objects.create(door_id = i, token_hex = hex)
 
-    return JsonResponse({'access_token' : hex})
+        return JsonResponse({'access_token' : hex})
 
 
 def validate(request, door_id, access_token):
